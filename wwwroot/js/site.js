c82d1971195a5a29362c8afb4d3841026d55023a
Juanzq10dev <juanzq10dev@gmail.com>
2023-11-11 13:31:09 -0400

    added update name to view

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,7 +7,60 @@ function validateTask() {
     if (taskName == "") {
         alert('Please enter a valid name');
         return false;
-    } 
+    }
 
-    return true; 
+    return true;
+}
+
+function deleteTask(taskId) {
+    fetch(`/Home/DeleteTask/${taskId}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(response => {
+        if (response.ok) {
+            location.reload();
+        } else {
+            alert("Failed to delete task");
+        }
+    })
+}
+
+function updateTaskName(taskId) {
+    let newName = document.getElementById(`blockquote ${taskId}`).innerText.trim();
+    fetch(`/Home/UpdateTask/${taskId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ Name: newName })
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Name updated succesfully")
+        } else {
+            alert("Could not update the task")
+        }
+    });
+}
+
+function showEditButton(blockquote) {
+    var taskId = blockquote.getAttribute('data-task-id');
+    var editButton = document.querySelector(`[data-task-id="${taskId}"] + .edit-button`);
+
+    if (blockquote.innerText !== blockquote.dataset.originalValue) {
+        editButton.classList.remove('d-none');
+    } else {
+        editButton.classList.add('d-none');
+    }
+}
+
+function enableSaveButton(taskId, blockquote) {
+    const saveButton = document.getElementById(taskId);
+    if (blockquote.innerText !== blockquote.dataset.originalValue) {
+        saveButton.disabled = false;
+    } else {
+        saveButton.disabled = true;
+    }    
 }
